834de072b7a34d3efa482836b4dfa54e50fb6ccf
Abdelrhman98 <abdelrhman.soleman98@gmail.com>
2022-03-13 10:05:07 +0200

    adding task details

--- tasks/export/services/index.js
+++ tasks/export/services/index.js
@@ -2,7 +2,6 @@ const SRGEN = require('../../../generators/serviceRepo/serviceRepoINH.gen')
 const fsHelper = require('../../../helpers/files/file')
 const {getAllProductsForServiceRepo} = require('../../../DB/dataExtractors/services.exec')
 const main = async()=>{
-    
     let allServices = await getAllProductsForServiceRepo()
     fsHelper.writeJson("./test.json", allServices)
 }
